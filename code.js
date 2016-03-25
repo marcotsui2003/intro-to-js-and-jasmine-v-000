b870baae0e8577703fc8056749268d5f2503957f
Marco Tsui <marcotsui2003@yahoo.com>
2016-03-25 09:14:14 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(fav) {
+  return "I love " + fav;
+}
+
+function shouting (word) {
+  return word.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
